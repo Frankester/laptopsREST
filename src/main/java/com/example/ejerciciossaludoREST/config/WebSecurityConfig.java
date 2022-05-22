59b4652743bb4246cea319a99a80d57c53e94ef9
Frankester <francotomascallero@gmail.com>
2022-05-22 19:53:13 -0300

    adding spring security

---
+++ src/main/java/com/example/ejerciciossaludoREST/config/WebSecurityConfig.java
@@ -0,0 +1,53 @@
+package com.example.ejerciciossaludoREST.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.firewall.HttpFirewall;
+import org.springframework.security.web.firewall.StrictHttpFirewall;
+
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/hello").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .and()
+                .httpBasic();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("frankester").password(passwordEncoder().encode("fran1234")).roles("USER")
+                .and()
+                .withUser("admin").password(passwordEncoder().encode("admin1234")).roles("USER","ADMIN");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public HttpFirewall localHTTPFirewall(){
+        StrictHttpFirewall firewall = new StrictHttpFirewall();
+
+        firewall.setAllowSemicolon(true);
+        firewall.setAllowBackSlash(true);
+
+        return firewall;
+    }
+}
